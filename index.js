8992ed375df186188e14ff878f9e9db37c52ddca
keno1122 <80107355+keno1122@users.noreply.github.com>
2021-03-19 11:41:19 +0100

    init

---
+++ index.js
@@ -0,0 +1,40 @@
+const dicsord = require("discord.js");
+const botconfig = require("./botconfig.json");
+
+const bot = new dicsord.Client();
+
+
+bot.on("ready", async () => {
+
+    console.log(`${bot.user.username} is online`)
+
+    bot.user.setActivity("testing", {type: "PLAYING"});
+
+});
+
+
+bot.on("message", async message => {
+
+    if(message.author.bot) return;
+ 
+
+    if(message.channel.type === "dm") return;
+
+    var prefix = botconfig.prefix;
+
+    var messageArray = message.content.split(" ");
+    
+    var comand = messageArray[0];
+
+    var arguments = messageArray.slice(1);
+
+    if(comand ===`${prefix}hallo`){
+
+        return message.channel.send("hallo");
+    
+    }
+
+});
+
+
+bot.login(botconfig.token);
